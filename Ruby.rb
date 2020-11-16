1ab693bf7b18de922e4f5ad14e34e1743b49937b
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-16 14:55:58 +0600

    Create  Ruby.rb

---
+++ Ruby.rb
@@ -0,0 +1,19 @@
+a = gets().chomp().to_f
+if a >= 0.00 && a <= 2000.00
+    printf("Isento\n");
+end
+if a >= 2000.01 && a <= 3000.00
+    b = a - 2000
+    c = b * 0.08
+    printf("R$ %.2f\n", c);
+end
+if a >= 3000.01 && a <= 4500.00
+    b = a - 3000
+    c = b * 0.18 + 1000 * 0.08
+    printf("R$ %.2f\n", c);
+end
+if a > 4500.00
+    b = a - 4500
+    c = b * 0.28 + 1500 * 0.18 + 1000 * 0.08
+    printf("R$ %.2f\n", c);
+end
